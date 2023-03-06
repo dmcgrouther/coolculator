c417c4af48a47526f6a16ebea5ade72e3318edac
david-mcgrouther <dbmcgrouther@gmail.com>
2023-03-06 15:44:42 -0800

    This commit adds decimal functionality

--- coolculator/src/App.tsx
+++ coolculator/src/App.tsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import {Decimal} from 'decimal.js';
 
 function App() {
 
   const [number, setNumber] = useState<string>("0");
+  // let x : Decimal = new Decimal(0.1)
+  // let y : Decimal = new Decimal(0.2);
+  // console.log(x.add(y).toString())
 
   return (
     <div className="App">
